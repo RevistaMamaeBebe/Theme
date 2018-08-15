936345aa0d0ad2e4aa1f8bfb4f269f0b58f6ee73
Michel Vieira <michelvieira@outlook.com>
2018-08-15 17:35:28 -0300

    Update

--- src/js/posts.js
+++ src/js/posts.js
@@ -8,15 +8,15 @@
         $functions;
 
     Posts.prototype.initPosts = function () {
-        $self.posts.filter('#main').find('article:eq(0)').addClass('col-lg-8 col-md-6');
-        $self.posts.filter('#main').find('article:eq(1), article:eq(2), article:eq(3)').addClass('col-lg-4 col-md-6').addClass('col-lg-3 col-md-6').each(function () {
+        $self.posts.filter('[data-posts="main"]').find('article:eq(0)').addClass('col-lg-8 col-md-6');
+        $self.posts.filter('[data-posts="main"]').find('article:eq(1), article:eq(2), article:eq(3)').addClass('col-lg-4 col-md-6').addClass('col-lg-3 col-md-6').each(function () {
             $functions.limitText($(this), 200);
         });
-        $self.posts.filter('#main').find('article:eq(4)').addClass('col-lg-9 col-md-6');
-        $self.posts.filter('#main').find('article:eq(5), article:eq(6), article:eq(7), article:eq(8), article:eq(9)').addClass('col-lg-3 col-md-6').each(function () {
+        $self.posts.filter('[data-posts="main"]').find('article:eq(4)').addClass('col-lg-9 col-md-6');
+        $self.posts.filter('[data-posts="main"]').find('article:eq(5), article:eq(6), article:eq(7), article:eq(8), article:eq(9)').addClass('col-lg-3 col-md-6').each(function () {
             $functions.limitText($(this), 85);
         });
-        $self.posts.filter('#sub').find('article').addClass('col-lg-6');
+        $self.posts.filter('[data-posts="sub"]').find('article').addClass('col-lg-6');
 
         $self.initAds();
 
@@ -25,8 +25,8 @@
     Posts.prototype.initAds = function () {
 
         // Add Ads
-        $self.posts.filter('#main').find('article:eq(0)').after('<div class="ads box" data-ads="box"></div>');
-        $self.posts.filter('#main').find('article:eq(3), article:eq(9)').after('<div class="ads wide" data-ads="wide"></div>');
+        $self.posts.filter('[data-posts="main"]').find('article:eq(0)').after('<div class="ads box" data-ads="box"></div>');
+        $self.posts.filter('[data-posts="main"]').find('article:eq(3), article:eq(9)').after('<div class="ads wide" data-ads="wide"></div>');
 
         $self.posts.find('.ads.box').wrap('<div class="col"></div>');
         $self.posts.find('.ads.wide').wrap('<div class="col-lg-12"></div>');
